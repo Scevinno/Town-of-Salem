3ccfa9f29f9ea14e677273927b75818062875591
Scevinno <nikolas.scevko@gmail.com>
2026-02-27 15:58:15 +0100

    Update 35 app.js

--- app.js
+++ app.js
@@ -430,7 +430,7 @@ function loadManageGames() {
 }
 
 function renderGameCard(game) {
-  const joinUrl = `${window.location.origin}/Town-of-Salem/?join=${lobby.code}`;
+  const joinUrl = `${window.location.origin}?join=${lobby.code}`;
 
   return `
     <div class="game-card">
@@ -2252,6 +2252,7 @@ window.addEventListener("load", () => {
   }
 
 });
+
 
 
 
